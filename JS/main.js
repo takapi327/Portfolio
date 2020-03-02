85595a3db843959b78b7505b195e65d940295828
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2020-03-02 18:07:27 +0900

    Add images and animations to icons
    アイコンに画像とアニメーション追加

--- JS/main.js
+++ JS/main.js
@@ -41,6 +41,49 @@ window.onload = ()=>{
             }
         });
     });
+
+  // アイコンのフェードインイベント
+  var effect_btm = 300; // 画面下からどの位置でフェードさせるか(px)
+  var effect_move = 50; // どのぐらい要素を動かすか(px)
+  var effect_time = 800; // エフェクトの時間(ms) 1秒なら1000
+
+  //親要素と子要素のcssを定義
+  $('.sublist').css({
+      opacity: 0
+  });
+  $('.sublist').children().each(function(){
+      $(this).css({
+          opacity: 0,
+          transform: 'translateY('+ effect_move +'px)',
+          transition: effect_time + 'ms'
+      });
+  });
+
+  // スクロールまたはロードするたびに実行
+  $(window).on('scroll load', function(){
+      var scroll_top = $(this).scrollTop();
+      var scroll_btm = scroll_top + $(this).height();
+      var effect_pos = scroll_btm - effect_btm;
+
+      //エフェクトが発動したとき、子要素をずらしてフェードさせる
+      $('.sublist').each( function() {
+          var this_pos = $(this).offset().top;
+          if ( effect_pos > this_pos ) {
+              $(this).css({
+                  opacity: 1,
+                  transform: 'translateY(0)'
+              });
+              $(this).children().each(function(i){
+                  $(this).delay(100 + i*200).queue(function(){
+                      $(this).css({
+                          opacity: 1,
+                          transform: 'translateY(0)'
+                      }).dequeue();
+                  });
+              });
+          }
+      });
+  });
   });
   function addSticky() {
     $('.slide').each(function() {
@@ -95,5 +138,3 @@ function mouseOff(){
   var obj3 = document.getElementById("img3");
   obj3.src = "images/test.png";
 }
-
-
